980e8f1aa32b7bf72b43fbdb9b09dec515d3dad0
Ari Becker <ari-becker@users.noreply.github.com>
2020-07-06 11:43:47 +0300

    Remove unnecessary double-nested loop

--- .githooks/check-format.sh
+++ .githooks/check-format.sh
@@ -3,10 +3,8 @@
 set -euo pipefail
 
 echo '[INFO][dhall-aws][.githooks/check-format.sh] Checking formatting...'
-for depth in $(seq "${deepest_level}" -1 1) ; do
-  while IFS= read -r -d '' dhall_file ; do
-    echo "[INFO][dhall-aws][check-format.sh] Checking: ${dhall_file} ..."
-    dhall format --check --inplace "${dhall_file}"
-  done < <(find . -mindepth "${depth}" -maxdepth "${depth}" -name '*.dhall' -print0)
-done
+while IFS= read -r -d '' dhall_file ; do
+  echo "[INFO][dhall-aws][check-format.sh] Checking: ${dhall_file} ..."
+  dhall format --check --inplace "${dhall_file}"
+done < <(find . -name '*.dhall' -print0)
 echo '[INFO][dhall-aws][.githooks/check-format.sh] Finished checking formatting successfully!'
