58c4e525d95615583371a0bf0d135d8727fdd3f2
Ari Becker <ari-becker@users.noreply.github.com>
2020-07-06 11:09:02 +0300

    Add more to log output

--- .githooks/check-format.sh
+++ .githooks/check-format.sh
@@ -2,12 +2,11 @@
 
 set -euo pipefail
 
-echo 'Checking formatting...'
-deepest_level=$(find . -name '*.dhall' | sed 's/[^\/]//g' | awk '{ print length }' | sort | tail -n 1)
+echo '[INFO][dhall-aws][.githooks/check-format.sh] Checking formatting...'
 for depth in $(seq "${deepest_level}" -1 1) ; do
   while IFS= read -r -d '' dhall_file ; do
-    echo "Checking: ${dhall_file} ..."
+    echo "[INFO][dhall-aws][check-format.sh] Checking: ${dhall_file} ..."
     dhall format --check --inplace "${dhall_file}"
   done < <(find . -mindepth "${depth}" -maxdepth "${depth}" -name '*.dhall' -print0)
 done
-echo 'Finished checking formatting successfully!'
+echo '[INFO][dhall-aws][.githooks/check-format.sh] Finished checking formatting successfully!'
