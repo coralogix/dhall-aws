7dd3debc6672d66cf44d2a12e2a9f3cd3cbc8568
Ari Becker <ari-becker@users.noreply.github.com>
2020-07-06 12:04:55 +0300

    Reformat format.sh logs, remove unnecessary double loop

--- format.sh
+++ format.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 
-echo 'Formatting...'
+echo '[INFO][dhall-aws][format.sh] Formatting...'
 deepest_level=$(find . -name '*.dhall' | sed 's/[^\/]//g' | awk '{ print length }' | sort | tail -n 1)
-for depth in $(seq "${deepest_level}" -1 1) ; do
-  while IFS= read -r -d '' dhall_file ; do
-    echo "Formatting: ${dhall_file} ..."
-    dhall format --inplace "${dhall_file}"
-  done < <(find . -mindepth "${depth}" -maxdepth "${depth}" -name '*.dhall' -print0)
-done
-echo 'Finished formatting successfully!'
+while IFS= read -r -d '' dhall_file ; do
+  echo "[INFO][dhall-aws][format.sh] Formatting: ${dhall_file} ..."
+  dhall format --inplace "${dhall_file}"
+done < <(find . -name '*.dhall' -print0)
+echo '[INFO][dhall-aws][format.sh] Finished formatting successfully!'
